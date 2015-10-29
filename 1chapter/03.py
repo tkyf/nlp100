0c64d9ccd586ddba19407c1acb8ba983602bdcfe
Takuya Fujino <tkyf@users.noreply.github.com>
2015-10-29 21:13:05 +0900

    Add 03.円周率

---
+++ 1chapter/03.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main():
+    sentence='Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+    words = sentence.split(' ')
+    list_of_number_of_characteres = [len(word) for word in words]
+    print(list_of_number_of_characteres)
+
+if __name__ == '__main__':
+    main()
+
