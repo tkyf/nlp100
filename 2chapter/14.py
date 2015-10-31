b38452ceeb17be121b2f3d2e83957d5a407b9c61
Takuya Fujino <tkyf@users.noreply.github.com>
2015-11-01 05:41:59 +0900

    Add 14. 先頭からN行を出力

---
+++ 2chapter/14.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main():
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: {} N".format(sys.argv[0]), file=sys.stderr)
+        sys.exit(1)
+
+    n = int(sys.argv[1])
+
+    with open("hightemp.txt", "r", encoding="utf-8") as f:
+        for L in zip(f, range(n)):
+            print(L[0].strip())
+
+if __name__ == '__main__':
+    main()
+
