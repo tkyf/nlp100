ca7d2ddeb560c3e81abff39674c61f91bf36155a
Takuya Fujino <tkyf@users.noreply.github.com>
2015-10-31 22:16:33 +0900

    Add 12. 1列目をcol1.txtに，2列目をcol2.txtに保存

---
+++ 2chapter/12.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main():
+    lines = []
+
+    with open("hightemp.txt", "r", encoding="utf-8") as f:
+        for L in f.readlines():
+            lines.append(L.split("\t"))
+
+    with open("col1.txt", "w", encoding="utf-8") as col1, \
+         open("col2.txt", "w", encoding="utf-8") as col2:
+        for line in lines:
+            col1.write(line[0] + "\n")
+            col2.write(line[1] + "\n")
+
+if __name__ == '__main__':
+    main()
+
+# 動作確認
+# $ cut hightemp.txt -f 1
+# $ cut hightemp.txt -f 2
