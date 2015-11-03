ea6758fba66da1b01f3b624104cd216e8006db29
Takuya Fujino <tkyf@users.noreply.github.com>
2015-11-03 21:08:07 +0900

    Add 17. １列目の文字列の異なり

---
+++ 2chapter/17.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main():
+
+    kind_of_strings = set()
+
+    with open("hightemp.txt", "r", encoding="utf-8") as f:
+        for L in f:
+            col1 = L.split("\t")[0]
+            kind_of_strings.add(col1)
+
+    print(kind_of_strings)
+
+
+if __name__ == '__main__':
+    main()
+
+# 動作確認
+# $ sort hightemp.txt | cut -f1  | sort | uniq
+
