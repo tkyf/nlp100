2ff821a4ad82dd6530c0db412508c0aa10584e0a
Takuya Fujino <tkyf@users.noreply.github.com>
2015-11-03 21:22:16 +0900

    Add 18. 各行を3コラム目の数値の降順にソート

---
+++ 2chapter/18.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main():
+
+    with open("hightemp.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    sorted_lines = sorted(lines, key=lambda line:float(line.split("\t")[2]),
+            reverse=True)
+
+    for line in sorted_lines:
+        print(line, end="")
+
+if __name__ == '__main__':
+    main()
+
+# 動作確認
+# $ sort -nrk3 hightemp.txt
+
