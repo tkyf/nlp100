d349191443695bc25bfba7737ba97c43a3d33bee
Takuya Fujino <tkyf@users.noreply.github.com>
2015-11-04 22:46:31 +0900

    Add 24. ファイル参照の抽出

---
+++ 3chapter/24.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main():
+    import sys
+    import io
+    import json
+    import re
+
+    # cp932では出力できない文字が含まれているため、出力エンコーディングをUTF-8に変更
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+
+    with open("england.json", "r", encoding="utf-8") as f:
+        j = json.load(f)
+
+    text = j["text"]
+
+    p = re.compile(r'\[\[(File|ファイル):(.+?)(\|)(.*)\]\]')
+    for L in text.split("\n"):
+        m = p.search(L)
+        if m:
+            print(m.group(2))
+
+if __name__ == '__main__':
+    main()
+
