1b3c63fc1f0cd7064c30d07932086bbcc4ccc50c
Takuya Fujino <tkyf@users.noreply.github.com>
2016-04-16 22:44:19 +0900

    Update 30 モジュールを呼ぶようにした

---
+++ 4chapter/30.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import io
+import sys
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def main():
+    import neko
+
+    text = neko.read_and_map()
+    print(text)
+
+    return
+
+if __name__ == '__main__':
+    main()
+
