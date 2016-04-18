39dde88062d0becb23d467aa93ceb5ed6e07e732
tkyf <tkyf@users.noreply.github.com>
2016-04-18 23:03:25 +0900

    Add Morphクラス

---
+++ 5chapter/neko2.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+FILE = 'neko.txt.cabocha'
+
+
+class Morph:
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
