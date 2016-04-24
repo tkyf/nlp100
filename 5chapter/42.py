25b6ff7825f1c3c1fef494b3bd033715816a414b
tkyf <tkyf@users.noreply.github.com>
2016-04-24 19:41:05 +0900

    関数名の誤りを修正

--- 5chapter/42.py
+++ 5chapter/42.py
@@ -10,7 +10,7 @@ from neko2 import read_and_make_chunks, Morph
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
-def make_phrase_pair_exclude_symbols(morphs: List[Morph]):
+def make_phrase_exclude_symbols(morphs: List[Morph]):
     phrase = ""
     for morph in morphs:
         if morph.pos != '記号':
@@ -22,9 +22,9 @@ def main():
     sentences = read_and_make_chunks()
     for chunks in sentences:
         for chunk in chunks:
-            src_phrase = make_phrase_pair_exclude_symbols(chunk.morphs)
+            src_phrase = make_phrase_exclude_symbols(chunk.morphs)
             if chunk.dst != -1:
-                dst_phrase = make_phrase_pair_exclude_symbols(chunks[chunk.dst].morphs)
+                dst_phrase = make_phrase_exclude_symbols(chunks[chunk.dst].morphs)
             else:
                 dst_phrase = ''
             print('{}\t{}'.format(src_phrase, dst_phrase))
