5f62c7f4ae8f70e78e1a2d0d05c8d6375e192284
tkyf <tkyf@users.noreply.github.com>
2016-04-24 19:36:21 +0900

    Add 42. 係り元と係り先の文節の表示

---
+++ 5chapter/42.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import io
+import sys
+from typing import List
+
+from neko2 import read_and_make_chunks, Morph
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+
+def make_phrase_pair_exclude_symbols(morphs: List[Morph]):
+    phrase = ""
+    for morph in morphs:
+        if morph.pos != '記号':
+            phrase += morph.surface
+    return phrase
+
+
+def main():
+    sentences = read_and_make_chunks()
+    for chunks in sentences:
+        for chunk in chunks:
+            src_phrase = make_phrase_pair_exclude_symbols(chunk.morphs)
+            if chunk.dst != -1:
+                dst_phrase = make_phrase_pair_exclude_symbols(chunks[chunk.dst].morphs)
+            else:
+                dst_phrase = ''
+            print('{} -> {}'.format(src_phrase, dst_phrase))
+
+
+if __name__ == '__main__':
+    main()
