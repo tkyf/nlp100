078ea7cab7a631265cc4371391b1da1c5eb4eda5
tkyf <tkyf@users.noreply.github.com>
2016-04-24 19:06:46 +0900

    関数をライブラリに移動

--- 5chapter/neko2.py
+++ 5chapter/neko2.py
@@ -77,3 +77,68 @@ def read_and_make_morphs():
             sentence.append(morph)
 
     return text
+
+
+def read_and_make_chunks():
+    sentences = __read_and_make_sentences(FILE)
+    for sentence in sentences:
+        yield __make_chunks_from_sentence(sentence)
+
+
+def __read_and_make_sentences(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        sentence = []
+        for law_line in f:
+            line = law_line.strip()
+            if line != 'EOS':
+                sentence.append(line)
+            else:
+                if sentence:
+                    yield sentence
+                    sentence = []
+
+
+def __make_chunks_from_sentence(sentence):
+    def is_chunk_head(phrase):
+        return phrase.startswith('* ')
+
+    def get_dst(chunk_head):
+        return int(chunk_head.split(' ')[2][:-1])
+
+    chunks = []
+    chunk = Chunk()
+    for line in sentence:
+        if is_chunk_head(line):
+            if chunk.morphs:
+                chunks.append(chunk)
+            chunk = Chunk()
+            chunk.dst = get_dst(line)
+        else:
+            chunk.morphs.append(__make_morph(line))
+    if chunk.morphs:
+        chunks.append(chunk)
+    __set_srcs(chunks)
+    return chunks
+
+
+def __set_srcs(chunks):
+    for i, chunk in enumerate(chunks):
+        dst = chunk.dst
+        if dst != -1:
+            chunks[dst].srcs.append(i)
+
+
+def __make_morph(line):
+    if '\t' not in line:
+        surface = ''
+        feature = line
+    else:
+        surface, feature = line.split('\t')
+
+    features = feature.split(',')
+    pos = features[0]
+    pos1 = features[1]
+    base = features[6]
+
+    morph = Morph(surface, base, pos, pos1)
+    return morph
