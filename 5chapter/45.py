bd6d0d72fccfcd8984c74c0a50eed2240eee8fab
tkyf <tkyfu7@gmail.com>
2016-05-08 23:10:21 +0900

    45. 動詞の格パターンの抽出（UNIXコマンド）

--- 5chapter/45.py
+++ 5chapter/45.py
@@ -13,8 +13,13 @@ def main():
     sentences = read_and_make_chunks()
     for sentence in sentences:
         case_patterns = extract_case_patterns(sentence)
-        print('\n'.join(case_patterns))
+        if case_patterns:
+            print('\n'.join(case_patterns))
 
-
+# ・コーパス中に頻出する述語と各パターンの組み合わせ
+# $  python 45.py > 45.txt
+# $ sort 45.txt | uniq - c | sort - r | head
+# ・「する」「見る」「与える」という動詞の各パターン
+# $ grep -E '^する[[:space:]]|^見る[[:space:]]|^与える[[:space:]]'  45.txt | sort | uniq -c | sort -r
 if __name__ == '__main__':
     main()
